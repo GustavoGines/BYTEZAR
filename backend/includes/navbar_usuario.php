ff893092107f5c43f4f79129689f2fa8e88bfb00
GustavoGines <ginesparker95@gmail.com>
2025-05-20 01:01:29 -0300

    Actualizacion 1.6 Base de datos, registro, login, sesiones, rutas de directorio online

--- backend/includes/navbar_usuario.php
+++ backend/includes/navbar_usuario.php
@@ -2,12 +2,13 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+
 echo '<ul class="nav navbar-nav navbar-right me-auto">';
 if (isset($_SESSION['usuario'])) {
-    echo '<li class=" nav-item" ><a class="nav-link" href="#"><i class="fa fa-user"></i> ' . htmlspecialchars($_SESSION['usuario']['nombre']) . '</a></li>';
-    echo '<li class=" nav-item"><a href="/BYTEZAR/backend/modules/auth/logout.php" class="logout-link"><i class="fa fa-sign-out"></i> Cerrar sesión</a></li>';
+    echo '<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-user"></i> ' . htmlspecialchars($_SESSION['usuario']['nombre']) . '</a></li>';
+    echo '<li class="nav-item"><a href="/backend/modules/auth/logout.php" class="logout-link"><i class="fa fa-sign-out"></i> Cerrar sesión</a></li>';
 } else {
-    echo '<li class="active nav-item"><a class="nav-link login-brillante" href="/BYTEZAR/login.html"><span>Login</span></a></li>';
+    echo '<li class="active nav-item"><a class="nav-link login-brillante" href="/login.html"><span>Login</span></a></li>';
 }
 echo '</ul>';
 ?>
